cea25821aaa70d1012c33bbe497fd56c5df216ea
Melissa McEwen <mgmcewen@gmail.com>
2025-01-09 14:07:49 -0600

    fixing

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'edge'
+		})
 	},
 	extensions: ['.svelte', '.md', '.svx'],
 	preprocess: [
