3dad5d962fc1be024da285e16da14838887f0850
Melissa McEwen <mgmcewen@gmail.com>
2025-04-02 10:26:38 -0500

    updating navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { TestTubeDiagonal, FlaskConical, Book, Sun, Moon, Menu, Clock } from 'lucide-svelte';
+	import { TestTubeDiagonal, FlaskConical, Book, Sun, Moon, Menu, Clock, TableProperties } from 'lucide-svelte';
 	import { browser } from '$app/environment';
 
 	export let theme: 'shimmer' | 'synthwave';
@@ -19,8 +19,9 @@
 			icon: Clock
 		},
 		{ href: '/ordinary', text: 'Build a routine from scratch', icon: TestTubeDiagonal },
-		{ href: '/ingredients', text: 'Ingredients Dictionary', icon: FlaskConical }
-	];
+		{ href: '/ingredients', text: 'Ingredients Dictionary', icon: FlaskConical },
+		{ href: '/ordinary/compare', text: 'Compare products', icon: TableProperties }
+		];
 </script>
 
 <nav class="navbar bg-base-100/50 backdrop-blur-sm fixed top-0 z-50">
@@ -33,11 +34,10 @@
 
 	<div class="navbar-end">
 		<div class="dropdown dropdown-end lg:hidden">
-			<label tabindex="0" class="btn btn-ghost">
+			<button tabindex="0" class="btn btn-ghost">
 				<Menu class="h-5 w-5" />
-			</label>
+			</button>
 			<ul
-				tabindex="0"
 				class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
 			>
 				<li>
